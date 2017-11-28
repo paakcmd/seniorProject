ca34946d1cb9c0dedef6b1fef0649c541c91a0f5
lamurtales <murmurrain@gmail.com>
2017-11-29 00:16:46 +0700

    New menus

--- navbar.php
+++ navbar.php
@@ -88,9 +88,11 @@ Follow me on Twitter @maridlcrmn
           <ul class="nav navbar-nav navbar-right">
             <li><a href="./inputForm.php">New Employee</a></li>
             <li><a href="./addSubject.php">Add Subject</a></li>
+            <li><a href="./allocate.php">Allocate Employees</a></li>
             <li><a href="./cancelClass.php">Cancellation</a></li>
             <li><a href="./makeupClass.php">Make Up</a></li>
-            <li><a href="./generateDocument.php">Generate Contract</a></li>
+            <li><a href="./generateDocument.php">Course List</a></li>
+            <li><a href="./generateDocument.php">Employee List</a></li>
 			 <li><a href=""><img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" class="img-circle img-profile" />Juju</a></li>
 
 <!--
